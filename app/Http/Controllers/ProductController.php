24ead15964cc8d77b882d121aacb42b5c9f57a55
ahmedkhaleel <duZhuqkyptu8barnuz>
2022-09-20 00:44:57 +0200

    Image Controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ImageUploadRequest;
+use App\Http\Requests\ProductCreateRequest;
 use App\Http\Resources\ProductResource;
 use App\Models\Product;
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
@@ -12,6 +13,7 @@ use Symfony\Component\HttpFoundation\Response;
 
 class ProductController extends Controller
 {
+
     public function index()
     {
         $products = Product::paginate();
@@ -22,21 +24,18 @@ class ProductController extends Controller
         $product = Product::findOrFail($id);
         return ProductResource::make($product);
     }
-    public function store(Request $request)
+    public function store(ProductCreateRequest $request)
     {
-        $file = $request->file('image');
-        $name = Str::random(10);
-        $url = Storage::putFileAs('images', $file, $name.'.'.$file->extension());
-        $product = Product::create([
-            'title' => $request->input('title'),
-            'image' => env('MYY_URL').$url,
-            'description' => $request->input('description'),
-            'price' => $request->input('price'),
-        ]);
+
+       $product = Product::create($request->only(['title', 'description','image','price']));
+
         return response($product, Response::HTTP_CREATED);
     }
     public function update(Request $request, $id)
     {
+        $product = Product::findOrFail($id);
+        $product->update($request->only(['title', 'description','image','price']));
+        return response($product, Response::HTTP_ACCEPTED);
 
     }
     public function destroy($id)
