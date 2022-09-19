c40eb603649b9fd70b2fd6f4e0dff39e8588d9e5
ahmedkhaleel <duZhuqkyptu8barnuz>
2022-09-19 22:50:16 +0200

    Product Routes

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\ProductResource;
+use App\Models\Product;
+
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class ProductController extends Controller
 {
-    //
+    public function index()
+    {
+        $products = Product::paginate();
+        return ProductResource::collection($products);
+    }
+    public function show($id)
+    {
+        $product = Product::findOrFail($id);
+        return ProductResource::make($product);
+    }
+    public function store(Request $request)
+    {
+
+    }
+    public function update(Request $request, $id)
+    {
+
+    }
+    public function destroy($id)
+    {
+        $product = Product::findOrFail($id);
+        $product->delete();
+        return response(null,Response::HTTP_NO_CONTENT);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
@@ -24,6 +25,7 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::put('user/password', [UserController::class, 'updatePassword']);
     Route::apiResource('users', UserController::class);
     Route::apiResource('roles', RoleController::class);
+    Route::apiResource('products', ProductController::class);
 });
 
 
